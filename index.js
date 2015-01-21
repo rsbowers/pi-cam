88f843f8ea9c4e3699fa6d10881d7f056a296d33
Rsbowers <rob.bowers@gmail.com>
2015-01-20 21:04:59 -0500

    node script

---
+++ index.js
@@ -0,0 +1,10 @@
+var exec = require('child_process').exec;
+
+var image_path = './pic-' + Date.now() + '.jpg';
+
+var cmd = 'raspivid -o ' + image_path + '';
+
+exec(cmd, function(error, stdout, stderr) {
+  // output is in stdout
+  console.log('Image Saved @ : ', image_path);
+});

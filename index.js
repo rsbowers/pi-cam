26ac31545173de572681f0d02cf825ecf34480a4
Rsbowers <rob.bowers@gmail.com>
2015-01-20 21:15:02 -0500

    node script

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ var exec = require('child_process').exec;
 
 var image_path = 'media/pic-' + Date.now() + '.jpg';
 
-var cmd = 'raspivid -o ' + image_path + '';
+var cmd = 'raspistill -o ' + image_path;
 
 exec(cmd, function(error, stdout, stderr) {
   // output is in stdout
-  console.log('Image Saved @ : ', image_path);
+  if(error) {
+    console.log('Error : ', error);
+  } else {
+    console.log('Image Saved @ : ', image_path);
+  }
 });

e0f2debabb6d91f6c09edb4645783d5ae41c11f7
Rsbowers <rob.bowers@gmail.com>
2015-02-01 12:48:26 -0500

    mod

---
+++ config/development.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+	aws: {
+		accessKeyId: "AKIAJIBDGALJRXD7WNDA",
+		secretAccessKey: "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"
+	}
+};
